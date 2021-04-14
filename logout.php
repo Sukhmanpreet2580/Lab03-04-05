b1799e10d94ff498270799361d5d9ba568967320
Sukhmanpreet <Sukhmanpreet.Singh@MyGeorgian.ca>
2021-04-14 18:21:02 -0400

    Lab

---
+++ logout.php
@@ -0,0 +1,12 @@
+<?php
+
+  session_start();
+  
+  // Logging out means just destroying the session variable 'user'
+
+  unset($_SESSION['user']);
+
+  // Then redirect with a success message
+  $_SESSION['succeses'][] = "You have been successfully logged out.";
+  header("Location: index.php");
+  exit();
